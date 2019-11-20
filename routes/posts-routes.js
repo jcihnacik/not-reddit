7fe802035a5e9e0ef7db5427d585d73e1e478085
john Meng <john.meng014@gmail.com>
2019-11-20 15:07:35 -0500

    finished the post api routes and the posts.js model

---
+++ routes/posts-routes.js
@@ -0,0 +1,60 @@
+var db = require("../models");
+
+// Routes
+
+// rout for getting all the post.
+module.exports = function (app) {
+    app.get("/api/posts", function (req, res) {
+        var query = {};
+        if (req.query.user_id) {
+            query.UserId = req.query.user_id;
+        }
+        db.Post.findAll({
+            where: query,
+            include: [db.User]
+        }).then(function (dbPost) {
+            res.json(dbPost);
+        })
+    })
+
+    // route for retrieve a single post
+    app.get("/api/posts/:id", function (req, res) {
+        db.Post.findOne({
+            where: {
+                id: req.params.id
+            },
+            include: [db.Post]
+        }).then(function (dbPost) {
+            res.json(dbPost);
+        });
+    });
+
+    // Post route for saving a new post
+    app.post("/api/posts", function (req, res) {
+        db.Post.create(req.body).then(function (dbPost) {
+            res.json(dbPost);
+        });
+    });
+
+    app.delete("/api/posts/:id", function (req, res) {
+        db.Post.destroy({
+            where: {
+                id: req.params.id
+            }
+        }).then(function (dbPost) {
+            res.json(dbPost);
+        });
+    });
+
+    app.put("/api/posts", function (req, res) {
+        db.Post.update(
+            req.body,
+            {
+                where: {
+                    id: req.body.id
+                }
+            }).then(function (dbPost) {
+            res.json(dbPost);
+        });
+    });
+}
