d5492d7c329ea96a52b3aae03c1ff5a2dd56a9e8
jcihnacik <jcihnacik@gmail.com>
2019-11-19 21:05:27 -0500

    added user model

---
+++ models/user.js
@@ -0,0 +1,24 @@
+module.exports = function(sequelize, DataTypes) {
+    var User = sequelize.define("User", {
+      // Giving the User model a name of type STRING
+      name: DataTypes.STRING
+    });
+  
+    User.associate = function(models) {
+      // Associating User with Posts
+      // When an User is deleted, also delete any associated Squeeks
+      User.hasMany(models.Squeek, {
+        onDelete: "cascade"
+      });
+    };
+  
+    User.associate = function(models) {
+        // Associating User with Posts
+        // When an User is deleted, also delete any associated Squeeks
+        User.hasMany(models.Post, {
+          onDelete: "cascade"
+        });
+      };
+    return User;
+  };
+  
