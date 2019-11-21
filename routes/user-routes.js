bd973e6986c614641696e69059a378473aa8bc1a
Benjamin Brier <ben.brier14@gmail.com>
2019-11-21 13:43:17 -0500

    added user-routes, quickly reformatted all routes

---
+++ routes/user-routes.js
@@ -0,0 +1,42 @@
+// Requirements
+const db = require("../models");
+
+// Routes
+module.exports = function(app) {
+  // This route will search the DB and find all writers
+  app.get("/api/users", function(req, res) {
+    db.Users.findAll({
+      include: [db.Post]
+    }).then(function(dbUser) {
+      res.json(dbUser);
+    });
+  });
+
+  // This route will search for one writer chosen by the user
+  app.get("/api/users/:id", function(req, res) {
+    db.Users.findOne({
+      where: {
+        id: req.params.id
+      },
+      include: [db.Post]
+    }).then(function(dbUser) {
+      res.json(dbUser);
+    });
+  });
+
+  app.post("/api/users", function(req, res) {
+    db.User.create(req.body).then(function(dbUser) {
+      res.json(dbUser);
+    });
+  });
+
+  app.delete("/api/users/:id", function(req, res) {
+    db.User.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(dbUser) {
+      res.json(dbUser);
+    });
+  });
+};
